31e501fe184fb157e51e35a4ca4d5d50e9464ef7
NPCompete <npcompete@npcompete-desktop.(none)>
2013-08-20 14:35:39 +0530

    updates on this project as on 20-aug

--- KioskMode/KioskMode/src/main/java/com/kioskmode/MainActivity.java
+++ KioskMode/KioskMode/src/main/java/com/kioskmode/MainActivity.java
@@ -18,12 +18,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onBackPressed() {
+
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onAttachedToWindow() {
+
+        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
+        super.onAttachedToWindow();
     }
+
+
     
 }
