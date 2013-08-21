72167f0bdd1e42d06a9303281cde592ea97eafe0
NPCompete <npcompete@npcompete-desktop.(none)>
2013-08-21 12:02:15 +0530

    hacking text

--- KioskMode/KioskMode/build/source/r/debug/com/kioskmode/R.java
+++ KioskMode/KioskMode/build/source/r/debug/com/kioskmode/R.java
@@ -10,31 +10,37 @@ package com.kioskmode;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        /**  From: file:/home/npcompete/Bijesh/AndroiStudioWorkspace/Kiosk-Mode/KioskMode/KioskMode/src/main/res/values/colors.xml 
+         */
+        public static final int green=0x7f040000;
+    }
     public static final class dimen {
         /**  From: file:/home/npcompete/Bijesh/AndroiStudioWorkspace/Kiosk-Mode/KioskMode/KioskMode/src/main/res/values/dimens.xml 
  From: file:/home/npcompete/Bijesh/AndroiStudioWorkspace/Kiosk-Mode/KioskMode/KioskMode/src/main/res/values-sw720dp-land/dimens.xml 
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090001;
+        public static final int hack=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         /**  From: file:/home/npcompete/Bijesh/AndroiStudioWorkspace/Kiosk-Mode/KioskMode/KioskMode/src/main/res/values/strings.xml 
          */
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /**  From: file:/home/npcompete/Bijesh/AndroiStudioWorkspace/Kiosk-Mode/KioskMode/KioskMode/src/main/res/values/styles.xml 
@@ -44,9 +50,9 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- KioskMode/KioskMode/src/main/java/com/kioskmode/MainActivity.java
+++ KioskMode/KioskMode/src/main/java/com/kioskmode/MainActivity.java
@@ -3,8 +3,10 @@ package com.kioskmode;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +18,24 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+//        marquee(((View)findViewById(R.id.hack)),getResources().getString(R.string.hello_world));
+    }
+
+    private void marquee(final View view,String text){
+          if(view instanceof TextView){
+
+
+
+
+
+                          TextView tv = (TextView)view;
+                          String s = tv.getText().toString();
+                          tv.setText(s+"\n"+getResources().getString(R.string.hello_world));
+
+
+
+
+          }
     }
 
     @Override
